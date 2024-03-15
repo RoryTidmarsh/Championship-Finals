6056951ebbb58c2c2a3ffd490094bdb9bf0bf7db
RoryTidmarsh <RoryTidmarsh@users.noreply.github.com>
2024-03-15 19:43:28 +0000

    addition of find_classes
    this finds all the championship classes at the show, returning a dataframe with the name and the link.

--- champ_placement.py
+++ champ_placement.py
@@ -53,49 +53,89 @@ class champ_placement:
             return elements_between
         
         
-        def recent_champ(self, months_ago=0, print_statement=True):
-            """
-            Finds the most recent championship competition within a specified range of months.
-        
-            Args:
-                self: Instance of the class containing the method.
-                months_ago (int, optional): Number of months ago to start searching for championships. Default is 0, representing the current month.
-                print_statement (bool, optional): Whether to print search progress and results. Default is True.
-        
-            Returns:
-                tuple or None: A tuple containing the link and name of the most recent championship competition if found, or None if no championship is found within the specified range.
-        
-            Description:
-                This function searches for the most recent championship competition within a specified range of months. It starts the search from the current month (or a specified number of months ago) and goes back up to 12 months. For each month, it retrieves the HTML soup corresponding to the competition data and checks if any competition contains the word "Championship" in its name. If a championship is found, it returns a tuple containing the link and name of the championship competition. If no championship is found within the specified range, it returns None. The function optionally prints search progress and results based on the value of the print_statement parameter.
-            """
-            max_months = 12  # Maximum number of months to go back
-            for i in range(months_ago, max_months + 1):
-                month_soup = self.month_soup(i)
-                Name = None
-                link = None
-        
-                for j in range(1, len(month_soup)):
-                    td_element = month_soup[j].find_all('td')[-1]
-        
-                    if "Championship" in td_element.text:
-                        Name = td_element.text
-                        link =  self.base_link[:-9] + month_soup[j].get('data-href')
-                        if print_statement ==True:
-                            print(f"Championship found in {td_element.text}, link {link}")
-                            
-                        return link, Name  # Exit the function once Championship is found
-                        break
-        
-                if Name is None:
-                    if print_statement==True:
-                        print(f"No competition with 'Championship' in the name was found for {i} months ago. Trying next month.")
-                    
-            if print_statement ==True:
-                print("No competition with 'Championship' in the name was found in the last", max_months, "months.")
-            return None 
+    def recent_champ(self, months_ago=0, print_statement=True):
+        """
+        Finds the most recent championship competition within a specified range of months.
+    
+        Args:
+            self: Instance of the class containing the method.
+            months_ago (int, optional): Number of months ago to start searching for championships. Default is 0, representing the current month.
+            print_statement (bool, optional): Whether to print search progress and results. Default is True.
+    
+        Returns:
+            tuple or None: A tuple containing the link and name of the most recent championship competition if found, or None if no championship is found within the specified range.
+    
+        Description:
+            This function searches for the most recent championship competition within a specified range of months. It starts the search from the current month (or a specified number of months ago) and goes back up to 12 months. For each month, it retrieves the HTML soup corresponding to the competition data and checks if any competition contains the word "Championship" in its name. If a championship is found, it returns a tuple containing the link and name of the championship competition. If no championship is found within the specified range, it returns None. The function optionally prints search progress and results based on the value of the print_statement parameter.
+        """
+        max_months = 12  # Maximum number of months to go back
+        for i in range(months_ago, max_months + 1):
+            month_soup = self.month_soup(i)
+            Name = None
+            link = None
+    
+            for j in range(1, len(month_soup)):
+                td_element = month_soup[j].find_all('td')[-1]
+    
+                if "Championship" in td_element.text:
+                    Name = td_element.text
+                    link =  self.base_link[:-9] + month_soup[j].get('data-href')
+                    if print_statement ==True:
+                        print(f"Championship found in {td_element.text}, link {link}")
+                        
+                    return link, Name  # Exit the function once Championship is found
+                    break
+    
+            if Name is None:
+                if print_statement==True:
+                    print(f"No competition with 'Championship' in the name was found for {i} months ago. Trying next month.")
+                
+        if print_statement ==True:
+            print("No competition with 'Championship' in the name was found in the last", max_months, "months.")
+        return None 
 
+    def find_classes(self, months_ago=0, print_statement=False):
+        """
+        Extracts information about championship classes from the agility plaza website and organises it into a pandas DataFrame.
+    
+        Args:
+            self: Instance of the class containing the method.
+            months_ago (int, optional): An integer indicating how many months ago the function should search for championship classes. Default is 0, representing the current month.
+            print_statement (bool, optional): A boolean indicating whether to print statements during the execution of the function. Default is False.
+    
+        Returns:
+            pandas.DataFrame: A DataFrame containing information about championship classes, including the class name, link, and height.
+    
+        Description:
+            This function first retrieves the link and name of the most recent championship competition by calling the 'recent_champ' method. It then sends an HTTP GET request to the retrieved link, parses the response content using BeautifulSoup, and locates all 'card-block' elements within the HTML soup. It iterates through each 'card-block' element to find the championship classes. For each class found, it extracts the class name and link and appends them to a list. After gathering all class information, it creates a pandas DataFrame with columns for the class name, link, and height. The height is derived from the second word in the class name. The function sets the DataFrame index to be composed of the first two words extracted from the class name. Finally, it returns the DataFrame containing the championship class information.
+        """
+        show_link, show_name = self.recent_champ(months_ago, print_statement)
         
+        response = requests.get(show_link)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        
+        # Finding the day and classes in that day
+        div_elements = soup.find_all("div", class_="card-block")
+        
+        class_data = []
+        for day_div in div_elements:
+            for a in day_div.find_all('a'):
+                if "Championship Jumping" in a.text:
+                    name = a.text
+                    link = "agilityplaza.com" + a.get('href')
+                    class_data.append((name, link))
+                elif "Championship Agility" in a.text:
+                    name = a.text
+                    link = "agilityplaza.com" + a.get('href')
+                    class_data.append((name, link))
+        
+        # Create a pandas DataFrame
+        df = pd.DataFrame(class_data, columns=['Class Name', 'Link'])
+        df['class number'] = df['Class Name'].apply(lambda x: ' '.join(x.split()[:1]))
+        df.set_index('class number', inplace=True)
+        df['Height'] = df['Class Name'].apply(lambda x: x.split()[1] if len(x.split()) >= 2 else None)
+        return df
     
 
 #ch = champ_placement()
-#print(ch.current_year_link)
+#ch.recent_champ(months_ago=2)
