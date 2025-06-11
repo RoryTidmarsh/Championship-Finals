ba63881b6e501e6f1c887b82bbe2306f8bee8cd3
RoryTidmarsh <151524572+RoryTidmarsh@users.noreply.github.com>
2025-06-11 09:52:31 +0100

    Non local port

--- app.py
+++ app.py
@@ -95,4 +95,5 @@ def internal_error(error):
     return jsonify({'success': False, 'error': 'Internal server error'}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    # app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
