e7c4568c729d2d78c2f518996e0ee3bae686ae86
Rory Tidmarsh <151524572+RoryTidmarsh@users.noreply.github.com>
2026-01-14 18:40:23 +0000

    Update api/models.py to only check frontend sends

--- src/api/models.py
+++ src/api/models.py
@@ -5,16 +5,22 @@ class getNearShowsResponse(BaseModel):
     """Response model for getting a list of nearby shows & their dates."""
     shows: list[dict[str, str]]
 
+# class initialiseClassInfoResponse(BaseModel):
+#     """Response model for initializing class info."""
+#     model_config = ConfigDict(arbitrary_types_allowed=True)
 
-class lookupIDsResponse(BaseModel):
-    """Response model for lookup IDs."""
-    
-    agilityID: str = Field(..., pattern=r"^\d+$", description="Agility round ID")
-    jumpingID: str = Field(..., pattern=r"^\d+$", description="Jumping round ID")
+#     agilityClass: ClassInfo
+#     jumpingClass: ClassInfo
 
-class initializeClassInfoResponse(BaseModel):
-    """Response model for initializing class info."""
-    model_config = ConfigDict(arbitrary_types_allowed=True)
-    
-    agilityClass: ClassInfo
-    jumpingClass: ClassInfo
+# class getClassIDsResponse(BaseModel):
+#     """Response model for getting class IDs."""
+#     agilityID: str = Field(..., pattern=r"^\d+$", description="Agility class ID")
+#     jumpingID: str = Field(..., pattern=r"^\d+$", description="Jumping class ID")
+
+# class updateClassInfoResponse(BaseModel):
+#     """Response model for updating class info."""
+#     model_config = ConfigDict(arbitrary_types_allowed=True)
+
+#     agilityClass: ClassInfo
+#     jumpingClass: ClassInfo
+
